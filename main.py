f668c171724b0ecc8c673d5ead046d57c9de2205
idkosilov <idkosilov@gmail.com>
2021-02-16 00:38:03 +0700

    ivan

--- main.py
+++ main.py
@@ -1,40 +1,41 @@
 from textblob import TextBlob
+from collections import Counter
 
 
-def flesch_index(asl: float, asw: float, lang: str):
+def readable_index(asl: float, asw: float, lang: str):
     """
-    Влас, эту функцию делаешь ты
+    Влас, эту функцию делаешь ты для русского и английского языка
     :param lang: язык текста
     :param asl: средняя длина предложения в словах
     :param asw: средняя длина слова в слогах
     :return: FRE = 206.835 − (1.3 × ASL) − (60.1 × ASW) - индекс удобочитаемости
     классификацию = текст хорошо читается (загугли классификацию)
     """
-    return index, text
+    return value
 
 
-def chech_language(text):
-    for symbol in text:
-        if ord("а") <= ord(symbol) <= ord("я"):
-            return True
-        elif ord("a") <= ord(symbol) <= ord("z"):
-            return False
+def metrics_text(text):
+    lang = text.detect_language()
+    sentences = len(text.sentences)
+    words = len(text.words)
+    syllables = 0
+    letters_counter = Counter(text.lower())
+    if lang == "ru":
+        for vowel in ["а", "у", "о", "ы", "и", "э", "я", "ю", "ё", "е"]:
+            syllables += letters_counter[vowel]
+    elif lang == "en":
+        for vowel in ["a", "e", "i", "o", "u", "y"]:
+            syllables += letters_counter[vowel]
+    return lang, sentences, words, syllables
 
 
 def main():
     text = TextBlob(input("Введите текст: "))
-    lang = text.detect_language()
-    print(f"Предложений: {0}")
-    print(f"Слов: {0}")
-    print(f"Слогов: {0}")
-    asl, asw = 0, 0
-    print(f"Средняя длина предложения в словах: {0}")
-    print(f"Средняя длина слова в слогах: {0}")
-    index, text = flesch_index(asl, asw, lang)
-    print(f"Индекс удобочитаемости Флеша: {index}")
-    print(text)
-    print(f"Тональность текста: {0}")
-    print(f"Объективнсоть: {0}")
+    lang, sentences, words, syllables = metrics_text(text)
+    asl = words / sentences
+    asw = syllables / words
+    value = readable_index(asl, asw, lang)
+    sentiment, subjectivity = text.sentiment if lang == "en" else text.translate(to="en").sentiment
 
 
 if __name__ == "__main__":
